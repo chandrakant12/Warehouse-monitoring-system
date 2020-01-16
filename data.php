df6a0cdeed86bc9d18f7966a519803fe904621c1
Akshay-Chhajed <43756437+Akshay-Chhajed@users.noreply.github.com>
2020-01-16 23:36:25 +0530

    Added json folder and fetch value in database open data.php

--- data.php
+++ data.php
@@ -1,7 +1,20 @@
 <?php
-$url="https://api.thingspeak.com/channels/946616/feeds.json?results=2";
+$url="./json/warehouse.json";
 $json = file_get_contents($url);
 $json = json_decode($json,true);
+$conn = mysqli_connect('localhost','root','','iicdc');
+$t=$json['temperature'];
+$v=$json['vibration'];
+$h=$json['humidity'];
+$g=$json['Gas_sensor'];
+$f=$json['IR_flame_sensor'];
+echo "<br>temperature:".$t;
+echo "<br>vibration:".$v;
+echo "<br>humidity:".$h;
+echo "<br>Gas sensor:".$g;
+echo "<br>IR flame sensor:".$f;
+$sql = "INSERT INTO sensors (temperature,vibration,humidity,gas,flame)VALUES (".$t.",".$v.",".$h.",".$g.",'".$f."')";
+mysqli_query($conn,$sql)
+	
 
-echo $json['channel'];
 ?>
