186ed76a0a716992ef51ef07de5d77a4b6a9970f
Akshay-Chhajed <43756437+Akshay-Chhajed@users.noreply.github.com>
2020-01-17 16:11:44 +0530

    feeds

--- data.php
+++ data.php
@@ -1,9 +1,9 @@
 <?php
-$url="./json/warehouse.json";
+$url="https://thingspeak.com/channels/962789/feed.json";
 $json = file_get_contents($url);
 $json = json_decode($json,true);
 $conn = mysqli_connect('localhost','root','','iicdc');
-$t=$json['temperature'];
+/*$t=$json['temperature'];
 $v=$json['vibration'];
 $h=$json['humidity'];
 $g=$json['Gas_sensor'];
@@ -12,9 +12,12 @@ echo "<br>temperature:".$t;
 echo "<br>vibration:".$v;
 echo "<br>humidity:".$h;
 echo "<br>Gas sensor:".$g;
-echo "<br>IR flame sensor:".$f;
-$sql = "INSERT INTO sensors (temperature,vibration,humidity,gas,flame)VALUES (".$t.",".$v.",".$h.",".$g.",'".$f."')";
-mysqli_query($conn,$sql)
-	
+echo "<br>IR flame sensor:".$f;*/
+$c=$json['feeds'];
+foreach ($c as $key => $value) {
+	$sql = "INSERT INTO sensors (temperature,vibration,humidity,gas,flame)VALUES (".$c[$key]['field1'].",".$c[$key]['field2'].",".$c[$key]['field3'].",".$c[$key]['field4'].",'".$c[$key]['field5']."')";
+mysqli_query($conn,$sql);
+}
+
 
 ?>
